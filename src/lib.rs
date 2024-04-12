05f7eeb0d3d4788eba5eeb2fcdce479ce083b608
hiive <a.rollings@hiive.com>
2024-04-12 09:59:41 -0400

    ran cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,7 @@ pub mod prelude {
     pub use crate::storage::sqlite_storage_container_impl::SqliteStorageContainer;
     pub use crate::storage::storage_container_trait::StorageContainer;
     pub use crate::storage::unique_id_trait::UniqueId;
-    pub use hiivelabs_rand_utils_lib::prelude::{convert_str_to_title_case, convert_str_to_underscore_case};
+    pub use hiivelabs_rand_utils_lib::prelude::{
+        convert_str_to_title_case, convert_str_to_underscore_case,
+    };
 }
